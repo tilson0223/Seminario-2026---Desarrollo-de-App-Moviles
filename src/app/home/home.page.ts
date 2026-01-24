c253ba0be0eb3560299d36fd0478b095ac5c9285
tilson <tilson.ariasa@pca.edu.co>
2026-01-23 21:38:51 -0500

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class HomePage {
+  darkMode = false;
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+
+    document.body.classList.toggle('dark-theme');
+  }
   constructor() {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { register } from 'swiper/element/bundle';
+
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+register();
 
 bootstrapApplication(AppComponent, {
   providers: [
